f8c546edf392938471fa404431f1412eb1ca7143
fathur0101 <158470277+fathur0101@users.noreply.github.com>
2024-02-01 21:03:44 +0700

    All.js

---
+++ All.js
@@ -0,0 +1,17 @@
+const elements = document.querySelectorAll('.elemenz');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = '1';
+      entry.target.style.transform = 'translateX(0)';
+    } else {
+      entry.target.style.opacity = '0';
+      entry.target.style.transform = 'translateX(100px)';
+    }
+  });
+}, { threshold: 0.3 });
+
+elements.forEach(element => {
+  observer.observe(element);
+});
